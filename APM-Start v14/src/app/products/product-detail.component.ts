eacb31237444bac53e315e44bbee620928788c05
Kirsten Jardine <kirsten.jardine@pragroup.co.uk>
2023-05-11 08:17:46 +0100

    Prefetching Data using Route Resolvers

--- APM-Start v14/src/app/products/product-detail.component.ts
+++ APM-Start v14/src/app/products/product-detail.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Product } from './product';
-import { ProductService } from './product.service';
+import { Product, ProductResolved } from './product';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,20 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit{
   pageTitle = 'Product Detail';
-  product: Product | null = null;
-  errorMessage = '';
+  product: Product | undefined;
+  errorMessage: string | undefined;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit():void{
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.getProduct(id);
-  }
-  getProduct(id: number): void {
-    this.productService.getProduct(id).subscribe({
-      next: product => this.onProductRetrieved(product),
-      error: err => this.errorMessage = err
-    });
+    const resolvedData: ProductResolved =
+      this.route.snapshot.data['resolvedData'];
+    this.errorMessage = resolvedData.error;
+    this.onProductRetrieved(resolvedData.product);
   }
 
   onProductRetrieved(product: Product): void {
